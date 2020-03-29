bd870b47abdc6fa1c5d55926682fc09e16a13334
Свистунова Марина Петровна <31745345+MarinaSvistunova@users.noreply.github.com>
2020-03-29 19:45:04 +0300

    Update settings.py

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -108,13 +108,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MIDDLEWARE_CLASSES = [
-  'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
